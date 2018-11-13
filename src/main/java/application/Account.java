8912978383ff6e018a17b51f4804d289fea908c2
Manish Seth <m.seth077@gmail.com>
2018-11-13 12:52:58 +0000

    Adds a Java to JSON converter and JSON to Java String

---
+++ src/main/java/application/Account.java
@@ -0,0 +1,34 @@
+package application;
+
+public class Account {
+	
+	String firstName;
+	String lastName;
+	int accountNumber;
+	
+	Account(String first, String second, int accountNo)
+	{
+		this.firstName =  first; 
+		this.lastName = second;
+		this.accountNumber = accountNo;
+	}
+	Account ()
+	{
+		
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	@Override
+	public String toString() {
+		return "Account [firstName=" + firstName + ", lastName=" + lastName + ", accountNumber=" + accountNumber + "]";
+	}
+}
